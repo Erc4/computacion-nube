39cb3f93daa0119c712509ab794cb7cf09a5f63a
Erc4 <ericamezcua3@gmail.com>
2025-09-23 13:25:36 -0700

    Update main.dart

--- practica_08/lib/main.dart
+++ practica_08/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:practica_08/src/main_drawer.dart';
+import 'package:practica_06/src/main_drawer.dart';
+
 
 void main() => runApp(MyApp());
 
